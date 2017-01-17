2c5b74a844e60fdb6c5ba71a7db0dcc5b73e8063
郭通 <guotong314@gmail.com>
2017-01-17 15:56:17 +0800

    current nav

---
+++ Example/GTRootKit/Classes/RootViewController/GTNavigateModule.h
@@ -0,0 +1,18 @@
+//
+//  GTNavigateModule.h
+//  GTRootKit
+//
+//  Created by 郭通 on 17/1/17.
+//  Copyright © 2017年 郭通. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GTNavigationViewController.h"
+
+@interface GTNavigateModule : NSObject
+
+@property (nonatomic, strong) GTNavigationViewController *currentNavigation;
+
++(id) sharedInstance;
+
+@end
---
+++ Example/GTRootKit/Classes/RootViewController/GTNavigateModule.m
@@ -0,0 +1,23 @@
+//
+//  GTNavigateModule.m
+//  GTRootKit
+//
+//  Created by 郭通 on 17/1/17.
+//  Copyright © 2017年 郭通. All rights reserved.
+//
+
+#import "GTNavigateModule.h"
+
+@implementation GTNavigateModule
+
++ (id)sharedInstance {
+    static GTNavigateModule *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    
+    return sharedInstance;
+}
+
+@end
